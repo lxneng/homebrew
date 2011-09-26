53edab6e03579384076afb5020ae89474ca4ecd6
Eric Lo <lxneng@gmail.com>
2011-09-26 11:55:39 +0800

    change libevent download link to github source

--- Library/Formula/libevent.rb
+++ Library/Formula/libevent.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Libevent < Formula
-  url "http://downloads.sourceforge.net/project/levent/libevent/libevent-2.0/libevent-2.0.14-stable.tar.gz"
+  url "https://github.com/downloads/libevent/libevent/libevent-2.0.14-stable.tar.gz"
   homepage 'http://www.monkey.org/~provos/libevent/'
   md5 'cac0f379da35d3b98f83ac16fcfe1df4'
-  head 'git://levent.git.sourceforge.net/gitroot/levent/levent'
+  head 'git://github.com/libevent/libevent.git'
 
   fails_with_llvm "Undefined symbol '_current_base' reported during linking.", :build => 2326
 
